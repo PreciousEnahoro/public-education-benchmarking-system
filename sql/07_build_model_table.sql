927029fcb1974edc2d9d9950a21cd34c89a09518
PreciousEnahoro <40008533+PreciousEnahoro@users.noreply.github.com>
2026-04-23 12:58:59 -0400

    Update 07_build_model_table.sql

--- sql/07_build_model_table.sql
+++ sql/07_build_model_table.sql
@@ -1,49 +1,4 @@
-CREATE TABLE grad1819 AS
-SELECT *
-FROM read_csv_auto('C:\Users\enaho\Downloads\Absenteeism\graduation1819.csv');
-
-CREATE TABLE graduation1819 AS
-SELECT
-    "NCES LEA ID" AS LEAID,
-    2019 AS year,
-
-    CASE
-        -- Top-coded
-        WHEN Value LIKE '>=%' THEN 
-            CAST(REPLACE(Value, '>=', '') AS DOUBLE)
-
-        -- Bottom-coded
-        WHEN Value LIKE '<=%' THEN 
-            CAST(REPLACE(Value, '<=', '') AS DOUBLE)
-
-        -- Range values like 90-94
-        WHEN Value LIKE '%-%' THEN
-            (
-                CAST(SPLIT_PART(Value, '-', 1) AS DOUBLE) +
-                CAST(SPLIT_PART(Value, '-', 2) AS DOUBLE)
-            ) / 2
-		WHEN Value LIKE 'S' THEN NULL
-        -- Normal numeric
-        ELSE CAST(Value AS DOUBLE)
-    END AS grad_rate_2019,
-
-    Denominator AS cohort_size_2019,
-
-    CASE
-        WHEN Value LIKE '>=%' 
-          OR Value LIKE '<=%'
-          OR Value LIKE '%-%'
-        THEN 1 ELSE 0
-    END AS grad_imputed_flag
-
-FROM grad1819
-WHERE
-    School IS NULL
-    AND Subgroup = 'All Students'
-    AND "Data Description" = 'Four-Year Adjusted-Cohort Graduation Rates';
-
-
-
+--Fixing the poverty table
 drop table poverty_lea_all_years
 
 CREATE OR REPLACE TABLE poverty_lea_all_years AS
@@ -72,35 +27,9 @@ LEFT JOIN saipe_clean s
 
 GROUP BY 1,2;
 
-SELECT
-  year,
-  COUNT(*) AS districts,
-  AVG(poverty_rate_5_17) AS avg_poverty
-FROM poverty_lea_all_years
-GROUP BY year
-ORDER BY year;
 
-select * from  demo_wide_all_years
+--Updating the demo t 
 
- select distinct RACE_ETHNICITY from ccd_demo_all_years 
- where pct_race > 0
- 
- 
-select DISTINCT urbanicity from lea_year_master --poverty_lea_all_years
-
- 
-CREATE TABLE demo_wide_all_years AS
-SELECT
-    LEAID,
-    year,
-    MAX(CASE WHEN RACE_ETHNICITY = 'Black or African American' THEN pct_race END) AS pct_black,
-    MAX(CASE WHEN RACE_ETHNICITY = 'Hispanic/Latino' THEN pct_race END) AS pct_hispanic,
-    MAX(CASE WHEN RACE_ETHNICITY = 'White' THEN pct_race END) AS pct_white,
-    MAX(CASE WHEN RACE_ETHNICITY = 'Asian' THEN pct_race END) AS pct_asian,
-    MAX(CASE WHEN RACE_ETHNICITY = 'American Indian or Alaska Native' or RACE_ETHNICITY = 'Native Hawaiian or Other Pacific Islander' or RACE_ETHNICITY = 'Not Specified' or RACE_ETHNICITY = 'Two or more races' THEN pct_race END) AS pct_other_race,
-    MAX(total_enrollment) AS total_enrollment
-FROM ccd_demo_all_years
-GROUP BY LEAID, year;
 
 
  drop TABLE lea_year_master
